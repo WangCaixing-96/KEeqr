5db53b6d2d8904e18c5bd73afa2408f41520dfcc
WangCaixing-96 <71259945+WangCaixing-96@users.noreply.github.com>
2023-03-19 22:56:29 +0800

    Update quadprog.R

--- R/quadprog.R
+++ R/quadprog.R
@@ -1,20 +1,3 @@
-###  Copyright (C) 1995-2010 Berwin A. Turlach <Berwin.Turlach@gmail.com>
-###
-###  This program is free software; you can redistribute it and/or modify
-###  it under the terms of the GNU General Public License as published by
-###  the Free Software Foundation; either version 2 of the License, or
-###  (at your option) any later version.
-###
-###  This program is distributed in the hope that it will be useful,
-###  but WITHOUT ANY WARRANTY; without even the implied warranty of
-###  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-###  GNU General Public License for more details.
-###
-###  You should have received a copy of the GNU General Public License
-###  along with this program; if not, write to the Free Software
-###  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
-###  USA.
-###
 solve.QP.compact <- function(Dmat, dvec, Amat, Aind, bvec, meq=0,
                              factorized=FALSE){  
   n     <- nrow(Dmat)
@@ -70,23 +53,8 @@ solve.QP.compact <- function(Dmat, dvec, Amat, Aind, bvec, meq=0,
        Lagrangian = res1$lagr,
        iact=res1$iact[1:res1$nact])   
 }
-###  Copyright (C) 1995-2010 Berwin A. Turlach <Berwin.Turlach@gmail.com>
-###
-###  This program is free software; you can redistribute it and/or modify
-###  it under the terms of the GNU General Public License as published by
-###  the Free Software Foundation; either version 2 of the License, or
-###  (at your option) any later version.
-###
-###  This program is distributed in the hope that it will be useful,
-###  but WITHOUT ANY WARRANTY; without even the implied warranty of
-###  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-###  GNU General Public License for more details.
-###
-###  You should have received a copy of the GNU General Public License
-###  along with this program; if not, write to the Free Software
-###  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
-###  USA.
-###
+
+
 solve.QP <- function(Dmat, dvec, Amat, bvec, meq=0, factorized=FALSE){  
 
   n     <- nrow(Dmat)
